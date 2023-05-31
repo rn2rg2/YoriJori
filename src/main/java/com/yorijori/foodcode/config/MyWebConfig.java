8deaa0c3e360e277d89990916d7b84424fc9096f
sgustjd2 <shin1133@naver.com>
2023-05-31 20:30:41 +0900

    reset css 추가

--- src/main/java/com/yorijori/foodcode/config/MyWebConfig.java
+++ src/main/java/com/yorijori/foodcode/config/MyWebConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.yorijori.foodcode.common.LoginCheckInterceptor;
-
 //자동으로 구성된 스프링 MVC구성을 변경없이 추가 작업을 하기 위해 사용
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
@@ -18,6 +16,6 @@ public class MyWebConfig implements WebMvcConfigurer {
 		 * .addPathPatterns("/**"); //
 		 * .excludePathPatterns("/index.do","/emp/login.do","/emp/spring/login",
 		 * "/board/list.do","/board/ajax/list.do","/images/**","/css/**","/js/**");
-		 */	}
+		 */ }
 
 }
