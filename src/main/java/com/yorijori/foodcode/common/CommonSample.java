eb3acb7967c5dc34c9c524a7c1d7d2db29de418e
'sgustjd2' <'shin1133@naver.com'>
2023-05-31 14:19:20 +0900

    Spring boot project

---
+++ src/main/java/com/yorijori/foodcode/common/CommonSample.java
@@ -0,0 +1,5 @@
+package com.yorijori.foodcode.common;
+
+public class CommonSample {
+
+}
---
+++ src/main/java/com/yorijori/foodcode/service/ServiceSampleImpl.java
@@ -0,0 +1,8 @@
+package com.yorijori.foodcode.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceSampleImpl implements ServiceSample {
+
+}
