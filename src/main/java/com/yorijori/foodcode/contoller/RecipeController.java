f99d1e76d352eea5b9eeb9a9752c9604a1724eb8
pcoeid <pcoeid4@naver.com>
2023-06-08 13:56:04 +0900

    레시피 QA 추가

--- src/main/java/com/yorijori/foodcode/contoller/RecipeController.java
+++ src/main/java/com/yorijori/foodcode/contoller/RecipeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/recipe")
 @Controller
 public class RecipeController {
+	@RequestMapping("/QA")
+	private String qaRecipe(Model model) {
+		return "thymeleaf/recipe/recipeQA";
+	}
 	@RequestMapping("/view")
 	private String viewRecipe(Model model) {
 		return "thymeleaf/recipe/recipeview";
