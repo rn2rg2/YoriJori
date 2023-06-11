c683ee8450ed673b86afa020a342fb4aa73d9d84
sgustjd2 <shin1133@naver.com>
2023-06-11 18:05:54 +0900

    전체적인 레이아웃 수정 + jpa 세팅 추가

--- src/main/java/com/yorijori/foodcode/contoller/IndexController.java
+++ src/main/java/com/yorijori/foodcode/contoller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	@RequestMapping("/main")
 	public String mainpage (Model model) {
-		return "thymeleaf/mainpage/index";
+		return "thymeleaf/index";
 		
 	}
 
