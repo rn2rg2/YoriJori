ec1b3b5845276ce500698c57da24460084578749
sgustjd2 <shin1133@naver.com>
2023-06-15 00:03:22 +0900

    thumnail 추가 / repo service 연결

--- src/main/java/com/yorijori/foodcode/repository/IngredientDAO.java
+++ src/main/java/com/yorijori/foodcode/repository/IngredientDAO.java
@@ -1,5 +1,19 @@
 package com.yorijori.foodcode.repository;
 
+import java.util.List;
+
+import com.yorijori.foodcode.jpa.entity.Ingredients;
+
 public interface IngredientDAO {
+
+	Ingredients insert(Ingredients ingredients);
+
+	void delete(int matlNo);
+
+	void update(Ingredients updatedata);
+
+	List<Ingredients> selectAll();
+
+	List<Ingredients> selectAllByPage(int pageNo, int pagePerCount);
 
 }
--- src/main/java/com/yorijori/foodcode/repository/IngredientDAOImpl.java
+++ src/main/java/com/yorijori/foodcode/repository/IngredientDAOImpl.java
@@ -1,5 +1,56 @@
 package com.yorijori.foodcode.repository;
 
-public class IngredientDAOImpl implements IngredientDAO {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+import com.yorijori.foodcode.jpa.entity.Ingredients;
+import com.yorijori.foodcode.jpa.repository.IngredientsRepository;
+
+@Repository
+public class IngredientDAOImpl implements IngredientDAO {
+	IngredientsRepository repository;
+
+	@Autowired
+	public IngredientDAOImpl(IngredientsRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public Ingredients insert(Ingredients ingredients) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int matlNo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Ingredients updatedata) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Ingredients> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Ingredients> selectAllByPage(int pageNo, int pagePerCount) {
+		// TODO Auto-generated method stub
+		PageRequest pageRequest = PageRequest.of(pageNo, pagePerCount, Sort.by(Sort.Direction.DESC, "matlNo"));
+		Page<Ingredients> page = repository.findAll(pageRequest);
+		List<Ingredients> list = page.getContent();
+		return list;
+	}
 }
