49be27ca5dc31d8c1a09293ec01b03e821c0bca2
taeng9 <xodus4321@naver.com>
2023-06-19 01:10:49 +0900

    댓글 테이블 수정후 댓글+대댓글 기능 추가

--- src/main/java/com/yorijori/foodcode/service/BoardCommentService.java
+++ src/main/java/com/yorijori/foodcode/service/BoardCommentService.java
@@ -7,6 +7,9 @@ import com.yorijori.foodcode.jpa.entity.BoardComment;
 public interface BoardCommentService {
 	BoardComment insert(BoardComment boardComment);
 	List<BoardComment> selectAll();
+	List<BoardComment> selectComment(int commNo);
 	int update(BoardComment boardComment);
 	int delete(BoardComment boardComment);
+	long countAll();
+
 }
