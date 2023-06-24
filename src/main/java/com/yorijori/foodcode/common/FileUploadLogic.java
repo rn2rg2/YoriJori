a24dc79a711124d477ea24c9a0e7946ce518f307
sgustjd2 <shin1133@naver.com>
2023-06-24 11:37:36 +0900

    레시피 등록

--- src/main/java/com/yorijori/foodcode/common/FileUploadLogic.java
+++ src/main/java/com/yorijori/foodcode/common/FileUploadLogic.java
@@ -70,7 +70,7 @@ public class FileUploadLogic {
 			try {
 				InputStream fileStream = image.getInputStream();
 				FileUtils.copyInputStreamToFile(fileStream, savefile);
-				String url = "/yorijori/data/recipeimage"+savedFileName;
+				String url = "/yorijori/data/recipeimage/"+savedFileName;
 				imglist.get(get).setStoreImg(url);
 				get++;
 			} catch (IOException e) {
