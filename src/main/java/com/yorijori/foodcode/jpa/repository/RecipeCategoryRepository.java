f7903a5cc2dda55e47299bbcf0650f1ed60b3c12
'sgustjd2' <'shin1133@naver.com'>
2023-07-03 17:52:37 +0900

    rcp admin

--- src/main/java/com/yorijori/foodcode/jpa/repository/RecipeCategoryRepository.java
+++ src/main/java/com/yorijori/foodcode/jpa/repository/RecipeCategoryRepository.java
@@ -2,8 +2,10 @@ package com.yorijori.foodcode.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.yorijori.foodcode.jpa.entity.Category;
 import com.yorijori.foodcode.jpa.entity.RecipeCategory;
 
 public interface RecipeCategoryRepository extends JpaRepository<RecipeCategory, Integer> {
+	long countByCategoryNo(Category categoryNo);
 
 }
